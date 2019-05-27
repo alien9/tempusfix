e486e3a509e1a2858a69f9108ca34455fa97a646
Tiago Barufi <barufi@gmail.com>
2019-05-27 09:00:40 -0300

    thumbnails done

---
+++ fix_thumb.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python2
+
+# -*- coding: utf-8 -*-
+
+
+import re,mysql.connector, datetime
+from db import *
+
+cursor = cnx.cursor(buffered=True)
+def set_thumb(post, p):
+	print("setting thumbnail to post %s to %s" % (post,p))
+	cursor.execute("select meta_id from wp_postmeta where meta_key='_thumbnail_id' and post_id=%s" % (post))
+	res=cursor.fetchall()
+	print(res)
+	if len(res)==0:
+		cursor.execute("insert into wp_postmeta (meta_key, post_id, meta_value) VALUES ('_thumbnail_id', %s, %s)" % (post, p))
+	else:
+		if len(res)>1:
+			print("tem muito thumbnail %s" % (post))
+		for k in res:
+			cursor.execute("UPDATE wp_postmeta set meta_value='%s' where meta_id=%s" % (p, k[0]))
+	cnx.commit()
+
+
+
+cursor.execute("SELECT p.id, p.post_content, p.post_title, p.post_type, m.meta_value from wp_posts p join wp_postmeta m on m.post_id=p.id where m.meta_key='air_video_link'", ())
+r=cursor.fetchall()
+for row in r:
+	print(row)
+	cursor.execute("select * from wp_posts where post_parent=%s and post_mime_type='image/png'" % (row[0]))
+	s=cursor.fetchone();
+	if s is not None:
+		print("FOUND %s" % (s[0]))
+		set_thumb(row[0], s[0])
+	
+"""
+	m=re.search('"([^"]+.png)"', row[1])
+	url = None
+	if m:
+		url=m.group(1)
+	else:
+		m=re.search('"([^"]+.jpe?g)"', row[1])
+		if m:
+			url=m.group(1)
+		else:
+			print("image not found %s" % (row[0]))
+	if url is not None:
+		print(url)
+		print("SELECT p.id from wp_posts p where p.guid='%s'"%(url))
+		cursor.execute("SELECT p.id from wp_posts p where p.guid='%s'"%(url))
+		s=cursor.fetchone()
+		
+		if s is not None:
+			print("id %s" % (s[0]))
+			id=s[0]
+		else:
+			date=datetime.datetime.now();
+			cursor.execute("INSERT INTO wp_posts (post_author, post_date, post_modified, post_date_gmt, post_modified_gmt,post_title, post_name, guid, post_type, post_mime_type, post_content, post_excerpt, to_ping, pinged, post_content_filtered) VALUES (1, '%s','%s','%s','%s','%s','%s','%s','attachment','image/png', '', '', '','','')" % (date, date, date, date, url, url, url))
+			cnx.commit()
+			id=cursor.lastrowid
+			print("insert %s" % (id))
+		print(id)
+		set_thumb(row[0], id)
+		
+	
+
+	cursor.execute("update wp_posts set post_type='portfolio-item' where id=%s"%(row[0]))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_post_gallery_layout', 'classic-gallery'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_gallery_fullwidth', 'off'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_post_primarygallery_layout', 'slider-gallery'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_gallery_layout', 'slider-gallery'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_show_infobutton', 'on'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_video_link', row[4]))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_show_aslightbox', 'on'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_featured_gallery_style', 'style-3x3'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_featured_gallery', 'off'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_preview_size', 'size-1x1'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_portfolio_title', 'on'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_portfolio_filters', 'a:1:{i:48;s:2:"48";}'))
+	cursor.execute("insert into wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, '%s', '%s')" % (row[0], 'tempus_title_style', 'titlestyle-left'))
+	cnx.commit()
+"""
